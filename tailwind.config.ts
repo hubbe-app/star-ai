f65c34253efad99e4669f4991974190b57813b4d
Vitor Hugo Foscarini dos Santos <vitor-hugo962@hotmail.com>
2023-10-07 14:33:36 -0300

    update tailwindconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,11 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundColor: {
+        'primary-button-hover': '#D980FA'
+      }
     },
   },
-  plugins: [],
+  plugins: []
 }
 export default config
