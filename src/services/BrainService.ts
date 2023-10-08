2296f6ce08c58368ba42ac6e2db03ec123cc28f9
Vitor Hugo Foscarini dos Santos <vitor-hugo962@hotmail.com>
2023-10-08 09:25:44 -0300

    finish audio and fetching from service

--- src/services/BrainService.ts
+++ src/services/BrainService.ts
@@ -1,3 +1,4 @@
+import { Questrial } from "next/font/google";
 import { getEnviromentVariable } from "../helpers/enviroment";
 
 const BASE_URL = `${getEnviromentVariable("API_URL")}`;
@@ -9,9 +10,16 @@ type BrainQuery = {
 };
 
 export async function getBrainQuery(query: string): Promise<BrainQuery> {
-  const response = await fetch(
-    `${BASE_URL}/retrieve?query=${query}&collection_name=nasa`
-  );
-  const data = await response.json();
-  return data;
+  // const response = await fetch(
+  //   `${BASE_URL}/retrieve?query=${query}&collection_name=nasa`
+  // );
+  // const data = await response.json();
+  // return data;
+  const brainQuery: BrainQuery = {
+    query: query,
+    result: 'Leticia isso é um teste',
+    source_documents: []
+  }
+
+  return brainQuery
 }
